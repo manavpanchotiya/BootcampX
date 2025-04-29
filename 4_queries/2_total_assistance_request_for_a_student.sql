4c69a17371a1e756199ec976905122786570a058
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-29 17:43:20 -0500

    added Queries 4

---
+++ 4_queries/2_total_assistance_request_for_a_student.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) AS total_assistance,students.name
+FROM students
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
